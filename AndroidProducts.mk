3256d927c40bdd46027ce830b09fd8cd153d5eb7
danielml <daniel@danielml.dev>
2023-11-26 19:14:48 +0100

    socrates: Initial device tree
    Change-Id: Iadc6c89fd4a25a487812fc9f64f2e906bbf9dc11
    Signed-off-by: danielml <daniel@danielml.dev>

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_socrates.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_socrates-eng \
+    lineage_socrates-user \
+    lineage_socrates-userdebug
---
+++ lineage_socrates.mk
@@ -0,0 +1,18 @@
+# Inherit common AOSP configurations
+$(call inherit-product, build/make/target/product/full_base_telephony.mk)
+$(call inherit-product, build/make/target/product/core_64_bit.mk)
+
+# Inherit device-specific configurations
+$(call inherit-product, device/xiaomi/socrates/device.mk)
+$(call inherit-product, device/xiaomi/socrates-prebuilt/socrates-prebuilt.mk)
+
+# Inherit LineageOS configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_socrates
+PRODUCT_DEVICE := socrates
+PRODUCT_MODEL := Redmi K60 Pro
+PRODUCT_BRAND := Redmi
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := Xiaomi/socrates/missi:13/TKQ1.220905.001/V14.0.25.0.TMKCNXM:user/release-keys
