ccebf2261596cbc97834d1d80aad7a8710241aa6
danielml <daniel@danielml.dev>
2023-12-29 23:24:57 +0100

    socrates: Set BOARD_EROFS_PCLUSTER_SIZE
    * As specified by AOSP documentation
    
    Change-Id: I6f82ff699cc47f7212b5f65ed30726b12b0010e7
    Signed-off-by: danielml <daniel@danielml.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,8 @@ $(foreach p, $(call to-upper, $(BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST))
     $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs) \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
+BOARD_EROFS_PCLUSTER_SIZE := 262144
+
 # Prebuilts
 include device/xiaomi/socrates-prebuilt/BoardConfigPrebuilt.mk
 
