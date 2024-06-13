f3d6bca0bd3af8b935b296768d3d3081c05c07b4
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    socrates: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- device.mk
+++ device.mk
@@ -97,7 +97,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.7.vendor \
     android.hardware.camera.common-V1-ndk.vendor \
     android.hardware.camera.device-V1-ndk.vendor \
-    android.hardware.camera.provider-V1-ndk.vendor
+    android.hardware.camera.provider-V1-ndk.vendor \
+    libexif.vendor \
+    libjpeg.vendor \
+    liblz4.vendor
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.aon@1.3.vendor \
@@ -143,7 +146,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # DumpState
 PRODUCT_PACKAGES += \
-    android.hardware.dumpstate-V1-ndk.vendor
+    android.hardware.dumpstate-V1-ndk.vendor \
+    libdumpstateutil.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
@@ -216,7 +220,8 @@ PRODUCT_PACKAGES += \
 
 # JSON
 PRODUCT_PACKAGES += \
-    libjson
+    libjson \
+    libjsoncpp.vendor
 
 # Keylayout
 PRODUCT_COPY_FILES += \
@@ -225,6 +230,14 @@ PRODUCT_COPY_FILES += \
 # Libchrome
 PRODUCT_PACKAGES += \
     libchrome.vendor
+
+# Libcurl
+PRODUCT_PACKAGES += \
+    libcurl.vendor
+
+# Libsqlite
+PRODUCT_PACKAGES += \
+    libsqlite.vendor
 
 # Light
 PRODUCT_PACKAGES += \
@@ -390,7 +403,8 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti \
     android.hardware.usb.gadget@1.2-service-qti \
     audio.usb.default \
-    usb_compositions.conf
+    usb_compositions.conf \
+    libusbhost.vendor
 
 # Vendor configurations
 $(call inherit-product, vendor/xiaomi/socrates/socrates-vendor.mk)
