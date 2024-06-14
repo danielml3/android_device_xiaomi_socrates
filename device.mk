515e6e363a93241ba3005c870c1afbcb2c447eaf
danielml <daniel@danielml.dev>
2024-06-14 13:29:13 +0200

    socrates: Import hardware/xiaomi soong namespace
    Change-Id: I1829e879826c1f9f288ae659693206bec1b932a2
    Signed-off-by: danielml <daniel@danielml.dev>

--- device.mk
+++ device.mk
@@ -53,7 +53,8 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/xiaomi
 
 # Telephony
 PRODUCT_PACKAGES += \
