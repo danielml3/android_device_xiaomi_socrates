a5e6e4548ceef48c3769e86bafd5a40735ee1cf4
danielml <daniel@danielml.dev>
2024-06-19 10:52:23 +0200

    socrates: Add keylayout for wakeup keys
    Change-Id: I94a7edf9f1835518d4eb0420b0159af226fc4188
    Signed-off-by: danielml <daniel@danielml.dev>

--- device.mk
+++ device.mk
@@ -218,6 +218,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/goodix_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix_ts.kl
+
 # Libchrome
 PRODUCT_PACKAGES += \
     libchrome.vendor
